fa95a668f3736473d48aea9e81e302ff7d40f9c7
Pyrotelekinetic <Pyrotelekinetic@users.noreply.github.com>
2017-12-27 22:16:52 -0700

    debugging

--- appuselfbot.py
+++ appuselfbot.py
@@ -378,7 +378,7 @@ async def restart(ctx):
             return (msg.content.lower().strip() == 'y' or msg.content.lower().strip() == 'n') and msg.author == bot.user
         else:
             return False
-
+"""
     latest = update_bot(True)
     if latest:
         await ctx.send(bot.bot_prefix + 'There is an update available for the bot. Download and apply the update on restart? (y/n)')
@@ -397,12 +397,12 @@ async def restart(ctx):
                 q.write('update')
             print('Downloading update and restarting...')
             await ctx.send(bot.bot_prefix + 'Downloading update and restarting (check your console to see the progress)...')
-
-    else:
-        print('Restarting...')
-        with open('restart.txt', 'w', encoding="utf8") as re:
-            re.write(str(ctx.message.channel.id))
-        await ctx.send(bot.bot_prefix + 'Restarting...')
+"""
+#    else:
+    print('Restarting...')
+    with open('restart.txt', 'w', encoding="utf8") as re:
+        re.write(str(ctx.message.channel.id))
+    await ctx.send(bot.bot_prefix + 'Restarting...')
 
     if bot.subpro:
         bot.subpro.kill()
